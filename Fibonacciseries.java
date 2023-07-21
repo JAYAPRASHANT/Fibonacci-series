5d962e7bce15c3dfb9e8a02499dcab57a6b0c8e4
JAYAPRASHANT <jayaprashanth.20eee@gmail.com>
2023-07-20 21:56:32 -0700

    prgm

---
+++ Fibonacciseries.java
@@ -0,0 +1,21 @@
+package Fibonacciseries;
+
+import java.util.Scanner;
+
+public class Fibonacciseries {
+	
+	
+	    static int fibonacci(int num){
+	        if (num<=1){
+	            return num;
+	        }
+	        return (fibonacci(num-1)+fibonacci(num-2));
+	    }
+	    public static void main(String[] args) {
+			System.out.println("Enter the number");
+			Scanner sc = new Scanner (System.in);
+			int num = sc.nextInt();
+			System.out.println(fibonacci(num));
+		}
+}
+	
